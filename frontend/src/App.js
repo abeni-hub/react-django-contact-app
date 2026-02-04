ea8a8614b09df33db24818399b62ca3368a425ff
abeni-hub <ebenezarsileshi@gmail.com>
2026-02-04 18:16:58 +0300

    add delete button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-
 function App() {
-
-  const [contacts,setContacts] = useState([]);
+  const [contacts, setContacts] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
-   useEffect(() => {
+  useEffect(() => {
     fetch("http://127.0.0.1:8000/api/contacts/")
       .then((response) => response.json())
       .then((data) => {
@@ -18,29 +16,37 @@ function App() {
   }, []);
 
   function handleAddContact() {
-  if (name.trim() === "" || email.trim() === "" || phone.trim() === "") {
-    return;
+    if (name.trim() === "" || email.trim() === "" || phone.trim() === "") {
+      return;
+    }
+
+    fetch("http://127.0.0.1:8000/api/contacts/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setContacts([...contacts, data]);
+        setName("");
+        setEmail("");
+        setPhone("");
+      });
+  }
+  function handleDeleteContact(id) {
+    fetch(`http://127.0.0.1:8000/api/contacts/${id}/`, {
+      method: "DELETE",
+    }).then(() => {
+      setContacts(contacts.filter((contact) => contact.id !== id));
+    });
   }
 
-  fetch("http://127.0.0.1:8000/api/contacts/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      phone: phone,
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setContacts([...contacts, data]);
-      setName("");
-      setEmail("");
-      setPhone("");
-    });
-}
 
   return (
     <div>
@@ -48,41 +54,43 @@ function App() {
       <p>Total contacts: {contacts.length}</p>
       <h2>Add Contact</h2>
 
-<input
-  type="text"
-  placeholder="Name"
-  value={name}
-  onChange={(e) => setName(e.target.value)}
-/>
-<br />
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
 
-<input
-  type="email"
-  placeholder="Email"
-  value={email}
-  onChange={(e) => setEmail(e.target.value)}
-/>
-<br />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
 
-<input
-  type="text"
-  placeholder="Phone"
-  value={phone}
-  onChange={(e) => setPhone(e.target.value)}
-/>
-<br />
+      <input
+        type="text"
+        placeholder="Phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <br />
 
-<button onClick={handleAddContact}>Add Contact</button>
+      <button onClick={handleAddContact}>Add Contact</button>
       <ul></ul>
-        {contacts.map((contact) => (
-          <li key={contact.id}>
-            <strong>{contact.name}</strong>
-            <br />
-            {contact.email} <br />
-            {contact.phone}
-          </li>
-        ))}
-
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <strong>{contact.name}</strong>
+          <br />
+          {contact.email} <br />
+          {contact.phone}
+          <button onClick={() => handleDeleteContact(contact.id)}>
+            Delete
+          </button>
+        </li>
+      ))}
     </div>
   );
 }
