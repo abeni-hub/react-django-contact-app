50a2495073db717cba8c18974bb3653be01c55e3
abeni-hub <ebenezarsileshi@gmail.com>
2026-02-04 16:24:28 +0300

    add use state and use effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
+
+  const [contacts,setContacts] = useState([]);
+
+   useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/contacts/")
+      .then((response) => response.json())
+      .then((data) => {
+        setContacts(data);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Contact List App</h1>
+      <p>Total contacts: {contacts.length}</p>
     </div>
   );
 }
